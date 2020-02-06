d352685915401aeb8f301bd1238a165602f2825b
Nguyễn Cao Trường Phong <phong.busbby@gmail.com>
2020-02-06 21:26:53 +0700

    added checkout page, checkout item component and updated cart to ise selectors from reselect

---
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './CheckoutItem.scss';
+
+const CheckoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <span className="remove-button">&#10005;</span>
+    </div>
+  );
+};
+
+export default CheckoutItem;
